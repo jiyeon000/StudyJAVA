aa754e2ccbae944f0b185c4ccdb8398874b3c41a
jiyeon000 <jiyeonkim324@gmail.com>
2022-04-22 16:43:30 +0900

    Chapter 4

---
+++ FlowEx1.java
@@ -0,0 +1,26 @@
+//public class FlowEx1 {
+//
+//    public static void main(String[] args) {
+//        int x = 0;
+//        System.out.printf("x=%d 일 때, 참인 것은 %n",x);
+//
+//        if(x==0) System.out.println("x==0");
+//        if(x!=0) System.out.println("x!=0");
+//        if(!(x==0)) System.out.println("!(x==0)");
+//        if(!(x!=0)) System.out.println("!(x!=0)");
+//
+//        x=1;
+//        System.out.printf("x=%d 일 때, 참인 것은 %n",x);
+//
+//        if(x==0) System.out.println("x==0");
+//        if(x!=0) System.out.println("x!=0");
+//        if(!(x==0)) System.out.println("!(x==0)");
+//        if(!(x!=0)) System.out.println("!(x!=0)");
+//
+//        for(int i=1; i<=3;i++)
+//            for(int j=1;j<=3;j++)
+//                for(int k=1;k<=3;k++)
+//                    System.out.println(""+i+j+k);
+//
+//    }
+//}
---
+++ FlowEx26.java
@@ -0,0 +1,14 @@
+//public class FlowEx26 {
+//
+//    public static void main(String[] args) {
+//        int sum = 0;
+//        int i = 0;
+//
+//        // i를 1씩 증가시켜서 sum에 계속 더해간다.
+//
+//        while((sum +=++i)<=100){
+//            System.out.printf("%d   -   %d%n", i, sum);
+//        }
+//        System.out.printf("%d   -   %d%n", i, sum);
+//    }
+//}
